f6fb67cad56f56a6fb09fdeec43cc61c1519f5f7
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-03-26 22:16:08 +0300

    Add files via upload

---
+++ src/Штанюк лр3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include "Automata.h"
+
+using namespace std;
+    
+int main() {
+    string menu[] = { "Tea", "Milk", "Americano", "Espresso", "Latte", "Cappuccino", "Hot chocolate" };
+    int price[] = { 3, 2, 4, 3, 4, 5, 3 };
+    Automata automat(menu, price);
+    automat.on();
+    automat.getMenu();
+    automat.coin(4);
+    automat.choice(6);
+
+    
+}
+
