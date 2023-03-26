98912ecdaa545a036dbb7206ee0e5df0d0f0131e
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-03-26 23:29:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <string>
 #include "Automata.h"
-    
+
 int main() {
-    std::string menu[] = { "Tea", "Milk", "Americano", "Espresso", "Latte", "Cappuccino", "Hot chocolate" };
+    std::string menu[] = { "Tea", "Milk", "Americano", "Espresso",
+                          "Latte", "Cappuccino", "Hot chocolate" };
     int price[] = { 3, 2, 4, 3, 4, 5, 3 };
     Automata automat(menu, price);
     automat.on();
     automat.getMenu();
     automat.coin(4);
-    automat.choice(6);   
+    automat.choice(6);  
 }
